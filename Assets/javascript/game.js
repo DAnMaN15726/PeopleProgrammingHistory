02609fe063521972351ef151a24771bbb1a6d12a
danman15726 <danman15726@gmail.com>
2019-10-06 22:08:13 -0500

    successfully fleshed out the question and answer logic in javascript. Also added a basic score-keeping system. Going to add a system that keeps track of how many tries/guesses left.

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,30 +1,85 @@
+let score = 0;
 var arrayA = ["Who is the one who created the C++ coding language?", "Who created the Java programming language?"];
+
+
+
+
+// Handles which question is asked //
 function theQuestion(){
 
     
     
     var Y = Math.floor(Math.random() * arrayA.length);
     var X = prompt(arrayA[Y]);
+    let arrayHold = [X , Y];
 
-    return (X, Y);
+    return arrayHold;
 }
 
-function theAnswer( X, Y){
-    var arrayB = ["A", "B"]
+
+
+
+// Determines what happens with the answer provided //
+function theAnswer(array){
+    var arrayB = ["bjarne stroustrup", "james gosling"]
     
-    let p = arrayB[Y];
-    if (p === "A" ){
+    let p = array[0].toLowerCase();
+    if (p === arrayB[array[1]] ){
+
         prompt("GJ!");
+        scoreKeep(true);
     }
     else{
+
         document.writeln("WRONG!");
+        return false;
+        scoreKeep(false);
+        
     }
 
-
+    theAnswer(theQuestion());
 
 
 
 }
+
+
+
+function scoreKeep(verdict){
+
+    if( verdict === true){
+
+        document.write(`Your score is: ${score = score + 1}`);
+
+
+    }
+    else{
+        
+        document.write(`You lost 1 point: ${score = score - 1}`);
+
+    }
+
+}
+
+
+
+
+function guessLeft(){
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 function hintSystem(){
@@ -44,5 +99,5 @@ function hintSystem(){
 
 
 
-
+// Basic start//
 theAnswer(theQuestion());
