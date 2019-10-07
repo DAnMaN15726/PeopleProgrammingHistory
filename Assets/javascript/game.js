c31a0ca44731658785ded1a43047266b3637a55f
danman15726 <danman15726@gmail.com>
2019-10-06 23:24:58 -0500

    A successful Retry/Guess system has been added. The only thing left for the logic side is the Hint system, Restart system and a Recent Answer Tracker. An array of sorts for the Hint & RAT System. A input check for the Restart function.

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -21,30 +21,51 @@ function theQuestion(){
 
 // Determines what happens with the answer provided //
 function theAnswer(array){
+    var counter = 3;
     var arrayB = ["bjarne stroustrup", "james gosling"]
     
-    let p = array[0].toLowerCase();
-    if (p === arrayB[array[1]] ){
+    while( counter !=0){
+        let p = array[0].toLowerCase();
+        if (p === arrayB[array[1]] ){
 
-        prompt("GJ!");
-        scoreKeep(true);
-    }
-    else{
-
-        document.writeln("WRONG!");
-        return false;
-        scoreKeep(false);
+            prompt("GJ!");
+            scoreKeep(true);
+            theAnswer(theQuestion());
+        }
+        else{
+            
+            
+            prompt("WRONG!");
         
-    }
+            scoreKeep(false);
+            counter = counter - 1;
+            prompt(`You have ${counter} tries left`);
+            if( counter === 0){
+                break;
+            }else{
+                // This particular code recalls the SAME QUESTION to be asked again. //
+                array[0] = prompt(arrayA[array[1]]);
+            }
 
-    theAnswer(theQuestion());
+            
+            
+            
+
+                
+            
+            
+            
+        }
+    }
+    return;
+    
 
 
 
 }
 
 
-
+// Updates the score //
 function scoreKeep(verdict){
 
     if( verdict === true){
@@ -55,20 +76,13 @@ function scoreKeep(verdict){
     }
     else{
         
-        document.write(`You lost 1 point: ${score = score - 1}`);
+        document.write(`You lost 1 point. Final score: ${score = score - 1}`);
 
     }
 
 }
 
 
-
-
-function guessLeft(){
-
-
-
-}
 
 
 
@@ -99,5 +113,5 @@ function hintSystem(){
 
 
 
-// Basic start//
+// Basic start. THIS IS WHERE THE CODE IS EXECUTED//
 theAnswer(theQuestion());
